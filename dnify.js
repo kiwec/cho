559a4b5e6f26d801afdaf83e6964985d67c6f8d4
Wolf Clément <c.wolf@kiwec.net>
2018-08-03 07:22:19 +0200

    Added emotes

--- dnify.js
+++ dnify.js
@@ -2,17 +2,47 @@
 let client = null;
 
 let characters = [
-	// [/a(?![^<]*>)/g, 'a_'],
+	[/'(?![^<]*>)/g, 'aps_'],
+	[/\.(?![^<]*>)/g, 'period_'],
+	[/th(?![^<]*>)/g, 'th_'],
+	[/ih(?![^<]*>)/g, 'ih_'],
+	[/ah(?![^<]*>)/g, 'ah_'],
+	[/dh(?![^<]*>)/g, 'dh_'],
+	[/ee(?![^<]*>)/g, 'ee_'],
+	[/ch(?![^<]*>)/g, 'ch_'],
+	[/ay(?![^<]*>)/g, 'ay_'],
+	[/sh(?![^<]*>)/g, 'sh_'],
+	[/oy(?![^<]*>)/g, 'oy_'],
+	[/oo(?![^<]*>)/g, 'oo_'],
+	[/j(?![^<]*>)/g, 'j_'],
+	[/h(?![^<]*>)/g, 'h_'],
+	[/f(?![^<]*>)/g, 'f_'],
+	[/m(?![^<]*>)/g, 'm_'],
+	[/a(?![^<]*>)/g, 'a_'],
+	[/b(?![^<]*>)/g, 'b_'],
+	[/d(?![^<]*>)/g, 'd_'],
+	[/s(?![^<]*>)/g, 's_'],
+	[/g(?![^<]*>)/g, 'g_'],
+	[/e(?![^<]*>)/g, 'e_'],
+	[/i(?![^<]*>)/g, 'i_'],
+	[/w(?![^<]*>)/g, 'w_'],
+	[/x(?![^<]*>)/g, 'x_'],
+	[/n(?![^<]*>)/g, 'n_'],
+	[/o(?![^<]*>)/g, 'o_'],
+	[/p(?![^<]*>)/g, 'p_'],
+	[/r(?![^<]*>)/g, 'r_'],
+	[/k(?![^<]*>)/g, 'k_'],
+	[/c(?![^<]*>)/g, 'c_'],
+	[/l(?![^<]*>)/g, 'l_'],
+	[/u(?![^<]*>)/g, 'u_'],
+	[/z(?![^<]*>)/g, 'z_'],
+	[/y(?![^<]*>)/g, 'y_'],
+	[/v(?![^<]*>)/g, 'v_'],
+	[/t(?![^<]*>)/g, 't_'],
 ];
 
 function init(cl) {
 	client = cl;
-
-	// Prints the list of server emojis in array format
-	console.log('\nPrinting available emojis...');
-	for(let emoji of client.emojis.array()) {
-		console.log(`[/${emoji.name.slice(0, -1)}(?![^<]*>)/g, '${emoji.name}'],`);
-	}
 
 	console.log('\nChecking required emotes availability...');
 	for(let character of characters) {
